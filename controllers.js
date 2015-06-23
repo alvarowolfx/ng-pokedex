c1c53a3fa873ad772374bae246d1bd914892c903
Alvaro Viebrantz <alvarowolfx@gmail.com>
2015-06-22 21:43:25 -0400

    Funcionalidade de busca de pokemons

--- controllers.js
+++ controllers.js
@@ -5,10 +5,13 @@
         PokemonsController.$inject = ['$scope', 'Pokemons'];
         function PokemonsController($scope, Pokemons){
             $scope.pokemons = [];
+            $scope.carregando = true;
             Pokemons.getAll().then(function(pokemons){
                 $scope.pokemons = pokemons;
+                $scope.carregando = false;
             },function(){
                 $scope.pokemons = [];
+                $scope.carregando = false;
             });
         }
 })();
